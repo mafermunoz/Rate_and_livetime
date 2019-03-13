55d7a0d9c04a7492a799b1d28aa18506dfd14592
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-03-13 18:50:48 +0100

    Update create_list_files.sh

--- create_list_files.sh
+++ create_list_files.sh
@@ -7,11 +7,13 @@ do
   for j in {1..12}
   do
     if [[ $j -lt 10 ]]; then
+        unset SEARCH_PATH
         SEARCH_PATH=$LIST_PATH$i"0"$j"*/*/*.root"
         echo $SEARCH_PATH
         #ls $SEARCH_PATH > "List_files"$i"0"$j
     else
-        SEARCH_PATH= $LIST_PATH$i$j"*/*/*.root"
+        unset SEARCH_PATH
+        SEARCH_PATH=$LIST_PATH$i$j"*/*/*.root"
         echo $SEARCH_PATH
         #ls $SEARCH_PATH > "List_files"$i$j
     fi
