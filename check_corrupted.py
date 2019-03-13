b28df90c42669797ec55313d999d7177d13b83bc
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-03-13 17:50:43 +0100

    Update check_corrupted.py

--- check_corrupted.py
+++ check_corrupted.py
@@ -11,27 +11,28 @@ badFiles = []
 
 for f in inF:
 	TF = ROOT.TFile(f,'READ')
-	TT = TF.Get("DmlNtup")
+	TT = TF.Get("livetime")
 
 	try:
 		nevt = TT.GetEntries()
 	except AttributeError:
+		print (f)
 		badFiles.append(f)
 		TF.Close()
 		continue
 
 
-	for n in range( nevt ):
-
-		pev = TT.GetEntry(n)
-
-		try:
-			argla = float(TT.MLscore_ntSelec_noFirstLay_full)
-		except AttributeError:
-			print "Error in file: ", f
-			badFiles.append(f)
-
-		break
+	# for n in range( nevt ):
+	#
+	# 	pev = TT.GetEntry(n)
+	#
+	# 	try:
+	# 		argla = float(TT.MLscore_ntSelec_noFirstLay_full)
+	# 	except AttributeError:
+	# 		print "Error in file: ", f
+	# 		badFiles.append(f)
+	#
+	# 	break
 
 	TF.Close()
 
