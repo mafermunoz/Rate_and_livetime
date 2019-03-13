a2b4c09ab54410cacffd6322be0ffd9084cb5290
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-03-13 15:55:06 +0100

    jc

---
+++ check_rate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+OUTPATH=/beegfs/dampe/users/mmunozsa/test_rate/
+
+for f in $(cat inFiles.txt)
+do
+	if [ ! -f ${f} ]; then
+		echo "File not found: " ${f}
+		continue
+	fi
+	BSN=$(basename ${f})
+	OUTF=${BSN/".root"/"_rate.root"}
+	if [ ! -f ${OUTPATH}/${OUTF} ]; then
+		echo "Submitting: " ${BSN}
+		sbatch submit_rate.sh ${f} ${OUTF} > /dev/null
+	fi
+done
---
+++ submit_rate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=rhel6-short
+#SBATCH --ntasks=1
+#SBATCH --mem=10G
+#SBATCH --job-name=ntSelec
+
+
+export HOME=/atlas/users/mmunozsa/
+
+source /cvmfs/dampe.cern.ch/rhel6-64/etc/setup.sh
+dampe_init > /dev/null
+
+./rate ${1} ${2}
