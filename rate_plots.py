dd73be7fea52c42e38171ca90c6d5f53d665e02d
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-03-20 18:15:20 +0100

    Create rate_plots.py

---
+++ rate_plots.py
@@ -0,0 +1,20 @@
+from root_numpy import root2array
+import numpy as np
+
+inF = [x for x in sys.argv if '.root' in x]
+
+time=[]
+trigger=[]
+saa=[]
+
+for i in inF:
+     a=root2array(i)
+     time.append(a['time_s']+a['time_ms']*0.001)
+     saa.append(a['saa'])
+     trigger.append(a['trigger'])
+
+
+
+
+info=np.stack((time,saa,trigger))    
+np.save("RATE_info.root",info)
