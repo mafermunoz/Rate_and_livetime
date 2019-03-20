f6d77c0e4348fbfae16d3199613b832e284a2434
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-03-20 16:02:30 +0100

    Update check_rate.sh

--- check_rate.sh
+++ check_rate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 OUTPATH=/beegfs/dampe/users/mmunozsa/test_rate/
 count=1
-for f in $(cat list_2019)
+for f in $(cat list_2015_2019)
 do
 	if [ ! -f ${f} ]; then
 		echo "File not found: " ${f}
@@ -12,7 +12,7 @@ do
 	if [ ! -f ${OUTPATH}/${OUTF} ]; then
 		echo "Submitting: " ${BSN}
 		sbatch submit_rate.sh ${f} ${OUTPATH}/${OUTF} > /dev/null
-		sleep 4
+		#sleep 4
 		#count=$count+1
 		#if [[ $ ]]; then
 			#statements
