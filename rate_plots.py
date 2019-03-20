48db2ce23d20f9ee2f345f2908fac48f6ed48cdc
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-03-20 22:50:22 +0100

    Update rate_plots.py

--- rate_plots.py
+++ rate_plots.py
@@ -15,26 +15,27 @@ time=[]
 days=60/60/24
 for i in inF:
     #branches=['time_s','time_ms','saa','trigger','sky_coord','energy','t_coord','c_coord'],selection='saa==1'
-     a=root2array(i,branches=['time_s','time_ms'],stop=100)
+     a=root2array(i,branches=['time_s','time_ms'])
 
      time.append(a['time_s']+a['time_ms']*0.001)
-     a=root2array(i,branches=['time_s','time_ms','saa'],selection='saa==1',stop=100)
+     a=root2array(i,branches=['time_s','time_ms','saa'],selection='saa==1')
      saa.append(a['time_s']+a['time_ms']*0.001)
-     a=root2array(i,branches=['time_s','time_ms','saa'],selection='saa==0',stop=100)
+     a=root2array(i,branches=['time_s','time_ms','saa'],selection='saa==0')
      no_saa.append(a['time_s']+a['time_ms']*0.001)
-     a=root2array(i,branches=['time_s','time_ms','saa','trigger'],selection='saa==0 && trigger[0]==1',stop=100)
+     a=root2array(i,branches=['time_s','time_ms','saa','trigger'],selection='saa==0 && trigger[0]==1')
      trigger_1.append(a['time_s']+a['time_ms']*0.001)
-     a=root2array(i,branches=['time_s','time_ms','saa','trigger'],selection='saa==0 && trigger[1]==1',stop=100)
+     a=root2array(i,branches=['time_s','time_ms','saa','trigger'],selection='saa==0 && trigger[1]==1')
      trigger_2.append(a['time_s']+a['time_ms']*0.001)
-     a=root2array(i,branches=['time_s','time_ms','saa','trigger'],selection='saa==0 && trigger[2]==1',stop=100)
+     a=root2array(i,branches=['time_s','time_ms','saa','trigger'],selection='saa==0 && trigger[2]==1')
      trigger_3.append(a['time_s']+a['time_ms']*0.001)
-     a=root2array(i,branches=['time_s','time_ms','saa','trigger'],selection='saa==0 && trigger[3]==1',stop=100)
+     a=root2array(i,branches=['time_s','time_ms','saa','trigger'],selection='saa==0 && trigger[3]==1')
      trigger_4.append(a['time_s']+a['time_ms']*0.001)
 
 
-print (time)
+
 time=np.array(time)/60/60/24
 time=time.astype(int)
+print (time)
 saa=np.array(saa)/60/60/24
 saa=saa.astype(int)
 no_saa=np.array(no_saa)/60/60/24
@@ -56,7 +57,7 @@ trigger_2a,trigger_2b=np.unique(trigger_2,return_counts=True)
 trigger_3a,trigger_3b=np.unique(trigger_3,return_counts=True)
 trigger_4a,trigger_4b=np.unique(trigger_4,return_counts=True)
 
-print time_a.shape
+print time_a
 
 
 info=np.stack((time_a,time_b,saa_a,saa_b,no_saa_a,no_saa_b,trigger_1a,trigger_1b,trigger_2a,trigger_2b,trigger_3a,trigger_3b,trigger_4a,trigger_4b))
