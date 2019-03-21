08ad415df552c8f9bf45d03eaefb07372f455a9c
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-03-21 01:51:19 +0100

    Update submit_rate_info.sh

--- submit_rate_info.sh
+++ submit_rate_info.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#SBATCH --partition=rhel6-veryshort
+#SBATCH --partition=rhel6-long
 #SBATCH --ntasks=1
-#SBATCH --mem=10G
+#SBATCH --mem=60G
 #SBATCH --job-name=Rate
 
 
 export HOME=/atlas/users/mmunozsa/
 
 source /cvmfs/dampe.cern.ch/rhel6-64/etc/setup.sh
+source  source ~/astro/bin/activate
 dampe_init > /dev/null
 
-./rate ${1} ${2}
+python rate_plot_filebyflie.py ${1}
