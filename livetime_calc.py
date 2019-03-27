6029f9cad910b5f6c7edc5d2c828e75d37e59cdb
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-03-27 16:12:15 +0100

    f

--- livetime_calc.py
+++ livetime_calc.py
@@ -6,13 +6,16 @@ inF = [x for x in sys.argv if '.root' in x]
 for i in inF:
      a=root2array(i)
      time=a['time_s']+a['time_ms']*0.001
-     delta_t=time[1:] -time[:-1]
+     delta_t=time[1:]-time[:-1]
      saa=np.logical_and(a['saa'][1:], a['saa'][:-1])
      time_avg=0.5*(time[1:]+time[:-1])
-     =0.5*(a[''][1:]+a[''][:-1])
+     sky_coord=a['sky_coord']
+     l=0.5*(sky_coord[3][1:]+sky_coord[3][:-1])
+     b=0.5*(sky_coord[4][1:]+sky_coord[4][:-1])
      lt_wosaa=0
      lt_saa=0
      lt_all=0
+     info=np.stack((delta_t,saa,l,b))
     for i,x in enumerate(delta_t):
         if (x<0.1 and x>0.00375 and saa[i]==True):
             lt_wsaa=lt_wsaa+(x-0.00375)
@@ -21,4 +24,4 @@ for i in inF:
         if (x<0.1 and x>0.00375):
             lt_all=lt_all+(x-0.00375)
 
-     np.save("delta_t"+str(i),delta_t)
+     np.save("Livetime"+str(i),lt_all,lt_wosaa,lt_waa,info)
--- second_based_info.py
+++ second_based_info.py
@@ -68,4 +68,4 @@ for i in inF:
      name=i.split("/")[-1].replace("rate.root","_rate_per_second.npy")
      name_2=i.split("/")[-1].replace("rate.root","_rate_per_second_pos_sky.npy")
      np.save(name,info)
-     np.save(name)
+     np.save(name,pos_sky)
