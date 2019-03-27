32100026e121fb5ac3b878b517795004ed52c1ab
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-03-27 16:20:13 +0100

    Update livetime_calc.py

--- livetime_calc.py
+++ livetime_calc.py
@@ -12,16 +12,37 @@ for i in inF:
      sky_coord=a['sky_coord']
      l=0.5*(sky_coord[3][1:]+sky_coord[3][:-1])
      b=0.5*(sky_coord[4][1:]+sky_coord[4][:-1])
-     lt_wosaa=0
-     lt_saa=0
-     lt_all=0
+     lt_wosaa=[]
+     lt_wosaa_l=[]
+     lt_wosaa_b=[]
+
+     lt_saa=[]
+     lt_saa_l=[]
+     lt_saa_b=[]
+
+     lt_all=[]
+     lt_all_l=[]
+     lt_all_b=[]
      info=np.stack((delta_t,saa,l,b))
     for i,x in enumerate(delta_t):
-        if (x<0.1 and x>0.00375 and saa[i]==True):
-            lt_wsaa=lt_wsaa+(x-0.00375)
-        if (x<0.1 and x>0.00375 and saa[i]==False):
-            lt_wosaa=lt_wosaa+(x-0.00375)
-        if (x<0.1 and x>0.00375):
-            lt_all=lt_all+(x-0.00375)
 
-     np.save("Livetime"+str(i),lt_all,lt_wosaa,lt_waa,info)
+        if (x<0.1 and x>0.00375 and saa[i]==True):
+            lt_wosaa.append((x-0.00375))
+            lt_wosaa_l.append(l[i])
+            lt_wosaa_b.append(b[i])
+
+
+        if (x<0.1 and x>0.00375 and saa[i]==False):
+            lt_saa.append((x-0.00375))
+            lt_saa_l.append(l[i])
+            lt_saa_b.append(b[i])
+
+        if (x<0.1 and x>0.00375):
+            lt_all.append((x-0.00375))
+            lt_all_l.append(l[i])
+            lt_all_b.append(b[i])
+     wosaa=np.stack((lt_wosaa,lt_wosaa_l,lt_wosaa_b))
+     saa=np.stack((lt_saa,lt_saa_l,lt_saa_b))
+     all=np.stack((lt_all,lt_all_l,lt_all_b))
+
+     np.save("Livetime"+str(i),wosaa,saa,all)
