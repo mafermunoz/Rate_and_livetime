c084003948912689cb2493fdb7654ab482a30c68
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-03-27 17:57:29 +0100

    Update livetime_calc.py

--- livetime_calc.py
+++ livetime_calc.py
@@ -29,24 +29,25 @@ for i in inF:
      print len(l)
      print len(b)
      #info=np.stack((delta_t,saa,l,b))
-     for i,x in enumerate(delta_t):
-         if (x<0.1 and x>0.00375 and saa[i]==True):
+     for j,x in enumerate(delta_t):
+         if (x<0.1 and x>0.00375 and saa[j]==True):
              lt_wosaa.append((x-0.00375))
-             lt_wosaa_l.append(l[i])
-             lt_wosaa_b.append(b[i])
+             lt_wosaa_l.append(l[j])
+             lt_wosaa_b.append(b[j])
 
 
-         if (x<0.1 and x>0.00375 and saa[i]==False):
+         if (x<0.1 and x>0.00375 and saa[j]==False):
              lt_saa.append((x-0.00375))
-             lt_saa_l.append(l[i])
-             lt_saa_b.append(b[i])
+             lt_saa_l.append(l[j])
+             lt_saa_b.append(b[j])
 
          if (x<0.1 and x>0.00375):
              lt_all.append((x-0.00375))
-             lt_all_l.append(l[i])
-             lt_all_b.append(b[i])
+             lt_all_l.append(l[j])
+             lt_all_b.append(b[j])
      wosaa=np.stack((lt_wosaa,lt_wosaa_l,lt_wosaa_b))
      saa=np.stack((lt_saa,lt_saa_l,lt_saa_b))
      all=np.stack((lt_all,lt_all_l,lt_all_b))
+     outpath="/beegfs/dampe/users/mmunozsa/livetime_per_month/"
      name=i.split("/")[-1].replace("rate.root","_livetime.npz")
-     np.savez(name,wosaa,saa,all)
+     np.savez(outpath+name,wosaa,saa,all)
