195bf5df5b6ecc78947fe82898fdd518db2b83b5
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-03-29 16:14:22 +0100

    Update rate.cc

--- rate.cc
+++ rate.cc
@@ -145,11 +145,12 @@ int main(int argc, char** argv){
       trigger[j]=evtheader->GeneratedTrigger(j);
 
     }
+    energy=bgorec->GetTotalEnergy()
 
-    DmpHKDSatStatus* sat = gHKDataReader->GetSatStatus(evtheader->GetSecond(), evtheader->GetMillisecond());
+    DmpHKDSatStatus* sat = gHKDataReader->GetSatStatus(time_s, time_ms);
     double pointcoord[4];
 
-    TVector3 v1(0.0,0.0,1);
+    TVector3 v1(0,0,1);
 
     sky_coord[4] = { };
     sat->GetParticleCoord(v1,sky_coord);
