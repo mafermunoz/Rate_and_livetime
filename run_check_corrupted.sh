dfa04a630a8ad283ff0d912344ce60de13a32a44
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-04-02 16:38:52 +0200

    Update run_check_corrupted.sh

--- run_check_corrupted.sh
+++ run_check_corrupted.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 LIST_PATH="/beegfs/dampe/users/mmunozsa/test_rate/DAMPE_2A_OBS_"
 
-for i in {2016..2016}
+for i in {2015..2019}
 do
   echo $i
-  for j in {6..12}
+  for j in {1..12}
   do
     if [[ $j -lt 10 ]]; then
         unset SEARCH_PATH
