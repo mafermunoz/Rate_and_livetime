ec5c4b1fd7c839a5867d530e4c387c02eb9a3e15
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-04-16 14:57:44 +0200

    Create second_based_pos.py

---
+++ second_based_pos.py
@@ -0,0 +1,34 @@
+import numpy as np
+import sys
+
+name=sys.argv[1]
+
+a=np.load(name+str("__rate_per_second_pos_sky_nosaa.npy"))#File ending _rate_per_second_pos_sky_nosaa.npy contianing the position information
+time=np.load(name+str("__rate_per_second_nosaa.npy"))#File endifit _rate_per_second_nosaa.npy events  per second
+ra=a[0]
+dec=a[1]
+lon=a[2]
+lat=a[3]
+
+##loop
+unique_values=time[4]
+indices_unique_values=time[6]
+number_repetitions=time[7]
+#start_count=time[5]
+d_ra=np.array([])
+d_dec=np.array([])
+d_lon=np.array([])
+d_lat=np.array([])
+for i in range (len(unique_values)):
+
+    #number_repetitions[i]
+    b=np.where[indices_unique_values==i]
+    d_ra=np.append(d_ra,np.average(ra[b[0]]))
+    d_dec=np.append(d_dec,np.average(dec[b[0]]))
+    d_lon=np.append(d_lon,np.average(lon[b[0]]))
+    d_lat=np.append(d_lat,np.average(lat[b[0]]))
+
+
+pos=np.vstack((d_ra,d_dec,d_lon,d_lat))
+
+np.save(name+str(averge_pos_per_second),pos)  
