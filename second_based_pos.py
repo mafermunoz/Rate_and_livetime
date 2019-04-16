5ddd2009d6094488e102e3d2dcd80a61a6726227
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-04-16 15:33:16 +0200

    Update second_based_pos.py

--- second_based_pos.py
+++ second_based_pos.py
@@ -6,13 +6,8 @@ name=sys.argv[1]
 a=np.load(name+str("__rate_per_second_pos_sky_nosaa.npy"))#File ending _rate_per_second_pos_sky_nosaa.npy contianing the position information
 time=np.load(name+str("__rate_per_second_nosaa.npy"))#File endifit _rate_per_second_nosaa.npy events  per second
 print a.shape
-ra=a[:][0]
-dec=a[:][1]
-lon=a[:][2]
-lat=a[:][3]
 
-print ra.shape
-##loop
+
 unique_values=time[4]
 print unique_values.shape
 indices_unique_values=time[6]
@@ -30,13 +25,11 @@ for i in range (len(unique_values)):
     #number_repetitions[i]
     b=np.where(indices_unique_values==i)
 
-    print b
-    print ra[b[0]]
-    print np.average(ra[b[0]])
-    d_ra=np.append(d_ra,np.average(ra[b[0]]))
-    d_dec=np.append(d_dec,np.average(dec[b[0]]))
-    d_lon=np.append(d_lon,np.average(lon[b[0]]))
-    d_lat=np.append(d_lat,np.average(lat[b[0]]))
+
+    d_ra=np.append(d_ra,np.average(a[b[0]][0]))
+    d_dec=np.append(d_dec,np.average(a[b[0]][1]))
+    d_lon=np.append(d_lon,np.average(a[b[0]][2]))
+    d_lat=np.append(d_lat,np.average(a[b[0]][3]))
 
 
 pos=np.vstack((d_ra,d_dec,d_lon,d_lat))
