61bb8efc8ebb4a94af0ef444a72f5da71949ced0
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-06-12 15:04:00 +0800

    c

--- plot_livetime.py
+++ plot_livetime.py
@@ -1,4 +1,3 @@
-#from root_numpy import root2array
 import numpy as np
 import sys
 
@@ -7,10 +6,8 @@ inF = [x for x in sys.argv if '_deltatime_nosaa.npz' in x]
 for i in inF:
     a=np.load(i)
     b=np.stack((a['delta_t'],a['l'],a['b']),axis=1)
-    delete a
     print (b.shape)
     time_st01=b[b[:,0]<0.1]
-    delete b
     time_lt00375=time_st01[time_st01[:,0]>0.00375]
     time_lt00375[0]=time_lt00375[0]-0.00375
     print(time_lt00375.shape)
