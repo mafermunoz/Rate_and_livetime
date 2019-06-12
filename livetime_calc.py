fffc8881ee98d90b54bc1f2e7273ddc76e5ffc7e
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-06-12 10:04:28 +0800

    Update livetime_calc.py

--- livetime_calc.py
+++ livetime_calc.py
@@ -5,7 +5,7 @@ import sys
 inF = [x for x in sys.argv if '.root' in x]
 
 for i in inF:
-     a=root2array(i)
+     a=root2array(i,branches=['time_s','time_ms','saa','sky_coord'])
      time=a['time_s']+a['time_ms']*0.001
      delta_t=time[1:]-time[:-1]
      saa=np.logical_and(a['saa'][1:], a['saa'][:-1])
@@ -14,9 +14,9 @@ for i in inF:
      sky_coord_b=a['sky_coord'][:,3]
      l=0.5*(sky_coord_l[1:]+sky_coord_l[:-1])
      b=0.5*(sky_coord_b[1:]+sky_coord_b[:-1])
-     lt_wosaa=[]
-     lt_wosaa_l=[]
-     lt_wosaa_b=[]
+     #lt_wosaa=[]
+     #lt_wosaa_l=[]
+     #lt_wosaa_b=[]
 
      #lt_saa=[]
      #lt_saa_l=[]
@@ -29,11 +29,11 @@ for i in inF:
      #print len(l)
      #print len(b)
      #info=np.stack((delta_t,saa,l,b))
-     for j,x in enumerate(delta_t):
-         if (x<0.1 and x>0.00375 and saa[j]==False):
-             lt_wosaa.append((x-0.00375))
-             lt_wosaa_l.append(l[j])
-             lt_wosaa_b.append(b[j])
+    # for j,x in enumerate(delta_t):
+    #     if (x<0.1 and x>0.00375 and saa[j]==False):
+    #         lt_wosaa.append((x-0.00375))
+    #         lt_wosaa_l.append(l[j])
+    #         lt_wosaa_b.append(b[j])
 
 
          #if (x<0.1 and x>0.00375 and saa[j]==True):
@@ -45,9 +45,9 @@ for i in inF:
         #     lt_all.append((x-0.00375))
         #     lt_all_l.append(l[j])
         #     lt_all_b.append(b[j])
-     wosaa=np.stack((lt_wosaa,lt_wosaa_l,lt_wosaa_b))
+     #wosaa=np.stack((lt_wosaa,lt_wosaa_l,lt_wosaa_b))
      #saa=np.stack((lt_saa,lt_saa_l,lt_saa_b))
      #all=np.stack((lt_all,lt_all_l,lt_all_b))
      outpath="/beegfs/dampe/users/mmunozsa/livetime_per_month/"
-     name=i.split("/")[-1].replace("rate.root","_livetime.npz")
-     np.savez(outpath+name,wosaa)#,saa,all)
+     name=i.split("/")[-1].replace("rate.root","_time_pos_average.npz")
+     np.savez(outpath+name,saa=saa,delta_t=delta_t,l=l,b=b)#,saa,all)
