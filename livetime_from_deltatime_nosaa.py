cb489e9e46eb84a2e769e8ef68c369465b5a10cb
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-06-12 18:03:13 +0800

    Update livetime_from_deltatime_nosaa.py

--- livetime_from_deltatime_nosaa.py
+++ livetime_from_deltatime_nosaa.py
@@ -12,12 +12,12 @@ for i in inF:
     time_st01=b[b[:,0]<0.1]
     del b
 
-    time_lt00375=time_st01[time_st01[:,0]>0.00375]
+    time_lt00375=time_st01[time_st01[:,0]>0.003075]
     del time_st01
-    time_lt00375[0]=time_lt00375[0]-0.00375
+    time_lt00375[0]=time_lt00375[0]-0.003075##0.003075
     print(time_lt00375.shape)
 
     outpath="/beegfs/dampe/users/mmunozsa/livetime_per_month/"
-    name=i.split("/")[-1].replace("_deltatime_nosaa.npz","_livetime_nosaa.npz")
+    name=i.split("/")[-1].replace("_deltatime_nosaa.npz","_livetime_nosaa2.npz")
     np.save(outpath+name,time_lt00375)
     del time_lt00375
