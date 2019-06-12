9214197c89dddc60e10d35121fd723e45c7ce1f4
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2019-06-12 11:36:08 +0800

    Create run_submit_livetime_from_deltatime_nosaa.sh

---
+++ run_submit_livetime_from_deltatime_nosaa.sh
@@ -0,0 +1,16 @@
+from root_numpy import root2array
+import numpy as np
+import sys
+
+inF = [x for x in sys.argv if '_deltatime_nosaa.npz' in x]
+
+for i in inF:
+    a=np.load(i)
+    time_st01=a[a['delta_t']<0.1]
+    time_lt00375=time_st01[time_st01['delta_t']>0.00375]
+    time_lt00375['delta_t']=time_lt00375['delta_t']-0.00375
+
+
+    outpath="/beegfs/dampe/users/mmunozsa/livetime_per_month/"
+    name=i.split("/")[-1].replace("_deltatime_nosaa.npz","_livetime_nosaa.npz")
+    np.save(outpath+name,a)
