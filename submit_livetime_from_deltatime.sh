71e43482d669d467b09cce8aa05dd9087fe69af1
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2020-08-26 12:22:32 +0200

    Update submit_livetime_from_deltatime.sh

--- submit_livetime_from_deltatime.sh
+++ submit_livetime_from_deltatime.sh
@@ -1,14 +1,21 @@
-#!/bin/bash
-#SBATCH --partition=rhel6-short
+#!/bin/sh
 #SBATCH --ntasks=1
-#SBATCH --mem=30G
-#SBATCH --job-name=Rate
+#SBATCH --mem=10G
+#SBATCH --job-name=rate
 
 
 export HOME=/atlas/users/mmunozsa/
 
-source /cvmfs/dampe.cern.ch/rhel6-64/etc/setup.sh
+source  /cvmfs/dampe.cern.ch/centos7/etc/setup.sh
+#source ~/astro/bin/activate
+dampe_init
+export LD_LIBRARY_PATH=/cvmfs/dampe.cern.ch/centos7/opt/DMPSW/latest/lib:${LD_LIBRARY_PATH}
+
+
+
+
+
 source ~/astro/bin/activate
-dampe_init > /dev/null
+
 
 python calc_livetime_from_deltatime.py ${1}
