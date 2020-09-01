33efe4a9983e2c7fa6017f3f395e971a084f51a3
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2020-09-01 16:20:59 +0200

    Update submit_livetime.sh

--- submit_livetime.sh
+++ submit_livetime.sh
@@ -6,10 +6,10 @@
 
 export HOME=/atlas/users/mmunozsa/
 
-source  /cvmfs/dampe.cern.ch/centos7/etc/setup.sh
+#source  /cvmfs/dampe.cern.ch/centos7/etc/setup.sh
 #source ~/astro/bin/activate
-dampe_init
-export LD_LIBRARY_PATH=/cvmfs/dampe.cern.ch/centos7/opt/DMPSW/latest/lib:${LD_LIBRARY_PATH}
+#dampe_init
+#export LD_LIBRARY_PATH=/cvmfs/dampe.cern.ch/centos7/opt/DMPSW/latest/lib:${LD_LIBRARY_PATH}
 
 
 python livetime_calc.py ${1}
