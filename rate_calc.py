d45e2010f45e97f3737884b45f6b9faa3ed3f1af
Maria Munoz <maria@Marias-MacBook-Pro-3.local>
2020-11-09 12:54:42 +0100

    Update rate_calc.py

--- rate_calc.py
+++ rate_calc.py
@@ -18,13 +18,13 @@ for i in inF:
      trigger=a['trigger']
 
      tt=np.average(time_avg)
-     print(trigger[0])
+     print(trigger[:,0])
 
-     t0=np.sum(trigger[0,:])
-     t1=np.sum(trigger[1,:])
-     t2=np.sum(trigger[2,:])
-     t3=np.sum(trigger[3,:])
-     t4=np.sum(trigger[4,:])
+     t0=np.sum(trigger[:,0])
+     t1=np.sum(trigger[:,1])
+     t2=np.sum(trigger[:,2])
+     t3=np.sum(trigger[:,3])
+     t4=np.sum(trigger[:,4])
 
 
 
